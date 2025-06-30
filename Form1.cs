dc632bc12064e21ace42c6abebef352f7d00d086
fartb <fartbreather228@gmail.com>
2025-06-30 19:19:25 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,22 @@
+namespace WF_1_Hw
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string
+                msg1 = "Heyo there, my name is Omina!",
+                msg2 = "I'm 17 y.o.",
+                msg3 = "I like to draw, sleep and eat.",
+                msg4 = "I really love my cats: Molly and Milky, and sometimes i think i would't even be able to imagine my life without them, because sometimes everything just proves that THEY are my life."; ;
+
+            MessageBox.Show(msg1, "My name", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show(msg2, "My age", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show(msg3, "My favourite thngs to do.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show(msg4, "Someones i love.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+        }
+    }
+}
